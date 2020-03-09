b3af12c5fd8d9f75d0330a21f6728da3e4a2d712
Strahinja Stefanović <stefanovic.strahinja97@gmail.com>
2020-03-09 12:54:56 +0100

    dz1z1

--- julia.c
+++ julia.c
@@ -1,174 +1,234 @@
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-# define DEFAULT_H 1000
-# define DEFAULT_W 1000
-# define DEFAULT_CNT 200
-# define DEFAULT_FILENAME "julia"
+#include <omp.h>
 
-int main (int argc, char *argv[]);
-unsigned char *julia_set ( int w, int h, int cnt, float xl, float xr, float yb, float yt );
-int julia ( int w, int h, float xl, float xr, float yb, float yt, int i, int j, int cnt );
-void tga_write ( int w, int h, unsigned char rgb[], char *filename );
-void timestamp ( );
+#define DEFAULT_H 1000
+#define DEFAULT_W 1000
+#define DEFAULT_CNT 200
+#define DEFAULT_FILENAME "julia"
 
-int main (int argc, char *argv[] )  {
-  int h = DEFAULT_H;
-  int w = DEFAULT_W;
-  int cnt = DEFAULT_CNT;
-  char filename[256] = DEFAULT_FILENAME; 
-  char buffer[256];
-  unsigned char *rgb;
-  double t1;
-  double t2;
-  float xl = - 1.5;
-  float xr = + 1.5;
-  float yb = - 1.5;
-  float yt = + 1.5;
+#define ACCURACY 0.01
 
-  if (argc == 4) {
-  h = atoi(argv[1]);
-  w = atoi(argv[2]);
-  cnt = atoi(argv[3]);
-  if (!h || !w || !cnt) return 1;
-  }
-  
-  strcat(filename, "_");
-  sprintf(buffer, "%d", h);
-  strcat(filename, buffer);
-  strcat(filename, "_");
-  sprintf(buffer, "%d", w);
-  strcat(filename, buffer);
-  strcat(filename, "_");
-  sprintf(buffer, "%d", cnt);
-  strcat(filename, buffer);
-  strcat(filename, ".tga");
-    
-  timestamp();
-  printf ( "\n" );
-  printf ( "JULIA Set\n" );
-  printf ( "  Plot a version of the Julia set for Z(k+1)=Z(k)^2-0.8+0.156i\n" );
+int main(int argc, char *argv[]);
+unsigned char *julia_set(int w, int h, int cnt, float xl, float xr, float yb, float yt);
+unsigned char *julia_set_parallel(int w, int h, int cnt, float xl, float xr, float yb, float yt);
+int julia(int w, int h, float xl, float xr, float yb, float yt, int i, int j, int cnt);
+void tga_write(int w, int h, unsigned char rgb[], char *filename);
+void timestamp(void);
 
-  rgb = julia_set ( w, h, cnt, xl, xr, yb, yt );
+int main(int argc, char *argv[])
+{
+	int h = DEFAULT_H;
+	int w = DEFAULT_W;
+	int cnt = DEFAULT_CNT;
+	char filename[256] = DEFAULT_FILENAME;
+	char buffer[256];
+	unsigned char *rgb;
+	unsigned char *rgbParallel;
+	double timeSequential;
+	double timeParallel;
+	float xl = -1.5;
+	float xr = +1.5;
+	float yb = -1.5;
+	float yt = +1.5;
 
-  tga_write ( w, h, rgb, filename );
+	if (argc == 4)
+	{
+		h = atoi(argv[1]);
+		w = atoi(argv[2]);
+		cnt = atoi(argv[3]);
+		if (!h || !w || !cnt)
+			return 1;
+	}
 
-  free ( rgb );
+	strcat(filename, "_");
+	sprintf(buffer, "%d", h);
+	strcat(filename, buffer);
+	strcat(filename, "_");
+	sprintf(buffer, "%d", w);
+	strcat(filename, buffer);
+	strcat(filename, "_");
+	sprintf(buffer, "%d", cnt);
+	strcat(filename, buffer);
+	strcat(filename, ".tga");
 
-  printf ( "\n" );
-  printf ( "JULIA set:\n" );
-  printf ( "  Normal end of execution.\n" );
+	// timestamp();
+	// printf("\n");
+	// printf("JULIA Set\n");
+	// printf("  Plot a version of the Julia set for Z(k+1)=Z(k)^2-0.8+0.156i\n");
 
-  timestamp();
-  
-  
-  return 0;
+	timeSequential = omp_get_wtime();
+	rgb = julia_set(w, h, cnt, xl, xr, yb, yt);
+	timeSequential = omp_get_wtime() - timeSequential;
+
+	timeParallel = omp_get_wtime();
+	rgbParallel = julia_set_parallel(w, h, cnt, xl, xr, yb, yt);
+	timeParallel = omp_get_wtime() - timeParallel;
+
+	printf("\n");
+	printf("Sequential execution time: %f\n", timeSequential);
+	printf("Parallel execution time: %f\n", timeParallel);
+
+	tga_write(w, h, rgbParallel, filename);
+
+	free(rgb);
+	free(rgbParallel);
+
+	// printf("\n");
+	// printf("JULIA set:\n");
+	// printf("  Normal end of execution.\n");
+
+	// timestamp();
+
+	return 0;
 }
 
-unsigned char *julia_set ( int w, int h, int cnt, float xl, float xr, float yb, float yt )
+unsigned char *julia_set(int w, int h, int cnt, float xl, float xr, float yb, float yt)
 {
-  int i;
-  int j;
-  int juliaValue;
-  int k;
-  unsigned char *rgb;
+	int i;
+	int j;
+	int juliaValue;
+	int k;
+	unsigned char *rgb;
 
-  rgb = ( unsigned char * ) malloc ( w * h * 3 * sizeof ( unsigned char ) );
+	rgb = (unsigned char *)malloc(w * h * 3 * sizeof(unsigned char));
 
-  for ( j = 0; j < h; j++ )
-  {
-    for ( i = 0; i < w; i++ )
-    {
-    juliaValue = julia ( w, h, xl, xr, yb, yt, i, j, cnt );
+	for (j = 0; j < h; j++)
+	{
+		for (i = 0; i < w; i++)
+		{
+			juliaValue = julia(w, h, xl, xr, yb, yt, i, j, cnt);
 
-    k = 3 * ( j * w + i );
+			k = 3 * (j * w + i);
 
-    rgb[k]   = 255 * ( 1 - juliaValue );
-    rgb[k+1] = 255 * ( 1 - juliaValue );
-    rgb[k+2] = 255;
-    }
-  }
- 
+			rgb[k] = 255 * (1 - juliaValue);
+			rgb[k + 1] = 255 * (1 - juliaValue);
+			rgb[k + 2] = 255;
+		}
+	}
 
-  return rgb;
+	return rgb;
 }
 
-int julia ( int w, int h, float xl, float xr, float yb, float yt, int i, int j, int cnt )
+unsigned char *julia_set_parallel(int w, int h, int cnt, float xl, float xr, float yb, float yt)
 {
-  float ai;
-  float ar;
-  float ci = 0.156;
-  float cr = -0.8;
-  int k;
-  float t;
-  float x;
-  float y;
+	int i;
+	int j;
+	int juliaValue;
+	int k;
+	unsigned char *rgb;
 
-  x = ( ( float ) ( w - i - 1 ) * xl
-      + ( float ) (     i     ) * xr ) 
-      / ( float ) ( w     - 1 );
+	rgb = (unsigned char *)malloc(w * h * 3 * sizeof(unsigned char));
 
-  y = ( ( float ) ( h - j - 1 ) * yb
-      + ( float ) (     j     ) * yt ) 
-      / ( float ) ( h     - 1 );
+	int threadID;
+	int numThreads;
+	int chunkSize;
 
-	  ar = x;
-  ai = y;
+	int start;
+	int end;
 
-  for ( k = 0; k < cnt; k++ )
-  {
-    t  = ar * ar - ai * ai + cr;
-    ai = ar * ai + ai * ar + ci;
-    ar = t;
+#pragma omp parallel default(none) private(i, j, k, juliaValue, threadID, start, end) \
+	shared(rgb, numThreads, chunkSize, w, h, xl, xr, yb, yt, cnt)
+	{
+		threadID = omp_get_thread_num();
+		numThreads = omp_get_num_threads();
+		chunkSize = (h + numThreads - 1) / numThreads;
 
-    if ( 1000 < ar * ar + ai * ai )
-    {
-      return 0;
-    }
-  }
+		start = threadID * chunkSize;
+		end = start + chunkSize < h ? start + chunkSize : h;
 
-  return 1;
+		for (j = start; j < end; j++)
+		{
+			for (i = 0; i < w; i++)
+			{
+				juliaValue = julia(w, h, xl, xr, yb, yt, i, j, cnt);
+
+				k = 3 * (j * w + i);
+
+				rgb[k] = 255 * (1 - juliaValue);
+				rgb[k + 1] = 255 * (1 - juliaValue);
+				rgb[k + 2] = 255;
+			}
+		}
+	}
+
+	return rgb;
 }
 
-void tga_write ( int w, int h, unsigned char rgb[], char *filename )
+int julia(int w, int h, float xl, float xr, float yb, float yt, int i, int j, int cnt)
 {
-  FILE *file_unit;
-  unsigned char header1[12] = { 0,0,2,0,0,0,0,0,0,0,0,0 };
-  unsigned char header2[6] = { w%256, w/256, h%256, h/256, 24, 0 };
+	float ai;
+	float ar;
+	float ci = 0.156;
+	float cr = -0.8;
+	int k;
+	float t;
+	float x;
+	float y;
 
-  file_unit = fopen ( filename, "wb" );
+	x = ((float)(w - i - 1) * xl + (float)(i)*xr) / (float)(w - 1);
 
-  fwrite ( header1, sizeof ( unsigned char ), 12, file_unit );
-  fwrite ( header2, sizeof ( unsigned char ), 6, file_unit );
+	y = ((float)(h - j - 1) * yb + (float)(j)*yt) / (float)(h - 1);
 
-  fwrite ( rgb, sizeof ( unsigned char ), 3 * w * h, file_unit );
+	ar = x;
+	ai = y;
 
-  fclose ( file_unit );
+	for (k = 0; k < cnt; k++)
+	{
+		t = ar * ar - ai * ai + cr;
+		ai = ar * ai + ai * ar + ci;
+		ar = t;
 
-  printf ( "\n" );
-  printf ( "TGA_WRITE:\n" );
-  printf ( "  Graphics data saved as '%s'\n", filename );
+		if (1000 < ar * ar + ai * ai)
+		{
+			return 0;
+		}
+	}
 
-  return;
+	return 1;
 }
 
-void timestamp ( void )
+void tga_write(int w, int h, unsigned char rgb[], char *filename)
 {
-# define TIME_SIZE 40
+	FILE *file_unit;
+	unsigned char header1[12] = {0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	unsigned char header2[6] = {w % 256, w / 256, h % 256, h / 256, 24, 0};
 
-  static char time_buffer[TIME_SIZE];
-  const struct tm *tm;
-  time_t now;
+	file_unit = fopen(filename, "wb");
 
-  now = time ( NULL );
-  tm = localtime ( &now );
+	fwrite(header1, sizeof(unsigned char), 12, file_unit);
+	fwrite(header2, sizeof(unsigned char), 6, file_unit);
 
-  strftime ( time_buffer, TIME_SIZE, "%d %B %Y %I:%M:%S %p", tm );
+	fwrite(rgb, sizeof(unsigned char), 3 * w * h, file_unit);
 
-  printf ( "%s\n", time_buffer );
+	fclose(file_unit);
 
-  return;
-# undef TIME_SIZE
+	// printf("\n");
+	// printf("TGA_WRITE:\n");
+	// printf("  Graphics data saved as '%s'\n", filename);
+
+	return;
+}
+
+void timestamp(void)
+{
+#define TIME_SIZE 40
+
+	static char time_buffer[TIME_SIZE];
+	const struct tm *tm;
+	time_t now;
+
+	now = time(NULL);
+	tm = localtime(&now);
+
+	strftime(time_buffer, TIME_SIZE, "%d %B %Y %I:%M:%S %p", tm);
+
+	printf("%s\n", time_buffer);
+
+	return;
+
+#undef TIME_SIZE
 }
